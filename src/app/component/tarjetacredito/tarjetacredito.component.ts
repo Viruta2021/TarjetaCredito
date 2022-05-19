b92112091771cc9e88f05244e316c181afda8553
Dio Jeria <diego-jeria@hotmail.com>
2022-05-18 21:24:29 -0400

    Se aplicaron cambios al a la tabla de listado tarjeta y cambios ts de angular

--- src/app/component/tarjetacredito/tarjetacredito.component.ts
+++ src/app/component/tarjetacredito/tarjetacredito.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjetacredito.component.css']
 })
 export class TarjetacreditoComponent implements OnInit {
+  listtarjeta:any[] = [
+  { titular: "juan perez", numeroTarjeta:"7636756", fechaexpiracion:"06/30",cvc:"33554"   },
+  { titular: "mili", numeroTarjeta:"667973", fechaexpiracion:"01/22",cvc:"5666"   },
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
